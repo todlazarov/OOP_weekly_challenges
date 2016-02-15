d8ef08aa6b41f4b0481b6965015cdb45a702dccc
Tod Lazarov <todlazarov1@gmail.com>
2016-02-15 16:22:32 -0500

    Refactor main logic

--- roman.rb
+++ roman.rb
@@ -1,60 +1,31 @@
 require 'pry'
 
 class Fixnum
+
+  ROMAN_NUMERALS = {
+    1000 => "M",
+    900 => "CM",
+    500 => "D",
+    400 => "CD",
+    100 => "C",
+    90 => "XC",
+    50 => "L",
+    40 => "XL",
+    10 => "X",
+    9 => "IX",
+    5 => "V",
+    4 => "IV",
+    1 => "I",
+  }
+
   def to_roman
-    "#{thousands}#{hundreds}#{tens}#{ones}"
-  end
-
-  def ones
     result = ''
-    remainder = self % 10
-    return 'IV' if remainder == 4
-    return 'IX' if remainder == 9
-    return 'V' if remainder == 5
-    if remainder < 5
-      remainder.times { result << 'I'}
-    elsif remainder > 5
-      result = 'V'
-      (remainder- 5).times {result << 'I'}
+    num = self
+    ROMAN_NUMERALS.each do |k, v|
+      result << v * (num / k)
+      num = num % k
     end
-    result
-  end
-
-  def tens
-    result = ''
-    remainder = (self % 100) / 10
-    return 'XL' if remainder == 4
-    return 'XC' if remainder == 9
-    return 'L' if remainder == 5
-    if remainder < 5
-      remainder.times {result << 'X'}
-    elsif remainder > 5
-      result = 'L'
-      (remainder - 5).times {result << 'X'}
-    end
-    result
-  end
-
-  def hundreds 
-    result = ''
-    remainder = (self % 1000) / 100
-    return 'CD' if remainder == 4
-    return 'CM' if remainder == 9
-    return 'D' if remainder == 5
-    if remainder < 5
-      remainder.times {result << 'C'}
-    elsif remainder > 5
-      result = 'D'
-      (remainder - 5).times {result << 'C'}
-    end
-    result
-  end
-
-  def thousands
-    result = ''
-    remainder = (self % 10000) / 1000
-    remainder.times {result << 'M'}
-    result
+    return result
   end
 
 end
