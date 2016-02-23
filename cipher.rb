36f1a1ef289dc343801e11c34ccee9c9fc9978c2
Tod Lazarov <todlazarov1@gmail.com>
2016-02-23 18:54:23 -0500

    Refactor main logic.

--- cipher.rb
+++ cipher.rb
@@ -1,68 +1,42 @@
 class Cipher
   attr_reader :key
 
-  ALPHABET = ('a'..'z').to_a
+  ALPHABET = [*?a..?z]
   MAX_CHARS = 100
 
   def initialize(key=nil)
     validate!(key)
-    @key = key || generate_key
+    @key = key || (ALPHABET.shuffle * 4).pop(MAX_CHARS).join
   end
 
   def encode(text)
     result = ''
     text.chars.each_with_index do |letter, index|
-      key_letter = find_key_letter(index)
-      distance_to_shift = find_distance_to_shift(key_letter)
-      new_letter = find_new_letter(letter, distance_to_shift)
-      result << new_letter
+      result << new_letter(letter, distance_to_shift(key[index]))
     end
-
     result
   end
 
   def decode(text)
     result = ''
     text.chars.each_with_index do |letter, index|
-      key_letter =find_key_letter(index)
-      distance_to_shift = find_distance_to_shift(key_letter)
-      original_letter = find_original_letter(letter, distance_to_shift)
-      result << original_letter
+      result << original_letter(letter, distance_to_shift(key[index]))
     end
-
     result
   end
 
   private
 
-  def find_key_letter(index)
-    key[index]
-  end
-
-  def find_distance_to_shift(key_letter)
+  def distance_to_shift(key_letter)
     ALPHABET.index(key_letter)
   end
 
-  def find_new_letter(current_letter, distance)
-    index = ALPHABET.index(current_letter)
-    if (index + distance) > (ALPHABET.size - 1)
-      return ALPHABET[(index + distance) - ALPHABET.size]
-    else
-      return ALPHABET[index + distance]
-    end
+  def new_letter(current_letter, distance)
+    ALPHABET[(ALPHABET.index(current_letter) + distance) % ALPHABET.size]
   end
 
-  def find_original_letter(current_letter, distance)
-    index = ALPHABET.index(current_letter)
-    if (index - distance) < 0
-      ALPHABET[ALPHABET.size - (distance - index)]
-    else
-      return ALPHABET[index - distance]
-    end
-  end
-
-  def generate_key
-    (ALPHABET.shuffle * 4).pop(100).join
+  def original_letter(current_letter, distance)
+    ALPHABET[(ALPHABET.index(current_letter) - distance) % ALPHABET.size]
   end
 
   def validate!(key)
